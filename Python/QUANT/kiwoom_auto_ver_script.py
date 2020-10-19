71e99a1da2aba0c7a2275c443e9a192afd5b6e34
mattblk <mattblk@naver.com>
2020-10-19 21:09:55 +0900

    kiwoom

--- Python/QUANT/kiwoom_auto_ver_script.py
+++ Python/QUANT/kiwoom_auto_ver_script.py
@@ -4,21 +4,28 @@ import time
 import os
 
 app = application.Application()
+# app.start("C:/Kiwoom/KiwoomFlash2/khministarter.exe") # Before Update
 app.start("C:\\KiwoomFlash3\\bin\\nkministarter.exe")
 
+# title = "번개 Login" # Before Update
 title = "번개3 Login"
-dlg = timings.wait_until_passes()(20, 0.5, lambda: app.window()(title=title))
+# dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title=title)) # Before Update
+dlg = timings.wait_until_passes(20, 0.5, lambda: app.window(title=title))
 
 pass_ctrl = dlg.Edit2
 pass_ctrl.set_focus()
-pass_ctrl.type_keys()('xxxxx')
+pass_ctrl.type_keys('xxxxx')
 
 cert_ctrl = dlg.Edit3
+# SetFocus() ▶ set_focus()
 cert_ctrl.set_focus()
-cert_ctrl.type_keys()('yyyy')
+# TypeKeys() ▶ type_keys()
+cert_ctrl.type_keys('yyyy')
 
 btn_ctrl = dlg.Button0
+# Click() ▶ click()
 btn_ctrl.click()
 
 time.sleep(50)
+# khmini.exe ▶ nkmini.exe
 os.system("taskkill /im nkmini.exe")
