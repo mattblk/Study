ce11884f26c30ef4b30771ba4a96b607ceb92dfc
mattblk <mattblk@naver.com>
2020-10-20 23:35:27 +0900

    201020

--- Python/QUANT/pytrader_window.py
+++ Python/QUANT/pytrader_window.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
-from pytrader_day1 import *
+from Kiwoom import *
 
 form_class = uic.loadUiType("Pytrader_01.ui")[0]
 
@@ -18,6 +18,24 @@ class MyWindow(QMainWindow, form_class):
         self.timer.start(1000)
         self.timer.timeout.connect(self.timeout)
 
+        self.timer2 = QTimer(self)
+        self.timer2.start(1000 * 10)
+        self.timer2.timeout.connect(self.timeout2)
+
+        #계좌정보 관련련
+        accouns_num = int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
+        accounts = self.kiwoom.get_login_info("ACCNO")
+
+        accounts_list = accounts.split(';')[0:accouns_num]
+        self.comboBox_Acc.addItems(accounts_list)
+
+        #종목코드 입력하면 호출되는 슬롯
+        self.lineEdit_Stock_Code.textChanged.connect(self.code_changed)
+        #주문 버튼
+        self.pushButton_order.clicked.connect(self.send_order)
+        #조회버튼
+        self.pushButton_look.clicked.connect(self.check_balance)
+
     def timeout(self):
         current_time = QTime.currentTime()
         text_time = current_time.toString("hh:mm:ss")
@@ -30,6 +48,70 @@ class MyWindow(QMainWindow, form_class):
             state_msg = "서버 미 연결 중"
 
         self.statusbar.showMessage(state_msg + " | " + time_msg)
+
+    def timeout2(self):
+        if self.checkBox.isChecked():
+            self.check_balance()
+
+    def code_changed(self):
+        code = self.lineEdit_Stock_Code.text()
+        name = self.kiwoom.get_master_code_name(code)
+        self.lineEdit_Stock_Code_2.setText(name)
+
+    def send_order(self):
+        order_type_lookup = {'신규매수': 1, '신규매도': 2, '매수취소': 3, '매도취소': 4}
+        hoga_lookup = {'지정가': "00", '시장가': "03"}
+
+        account = self.comboBox_Acc.currentText()
+        order_type = self.comboBox_order_type.currentText()
+        code = self.lineEdit_Stock_Code.text()
+        hoga = self.comboBox_hoga.currentText()
+        num = self.spinBox_num.value()
+        price = self.spinBox_price.value()
+
+        self.kiwoom.send_order("send_order_req", "0101", account, order_type_lookup[order_type], code, num, price,hoga_lookup[hoga], "")
+
+    def check_balance(self):
+        self.kiwoom.reset_opw00018_output()
+        account_number = self.kiwoom.get_login_info("ACCNO")
+        account_number = account_number.split(';')[0]
+
+        self.kiwoom.set_input_value("계좌번호", account_number)
+        self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
+
+        while self.kiwoom.remained_data:
+            time.sleep(0.2)
+            self.kiwoom.set_input_value("계좌번호", account_number)
+            self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 2, "2000")
+
+        # opw00001
+        self.kiwoom.set_input_value("계좌번호", account_number)
+        self.kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
+
+        # balance
+        item = QTableWidgetItem(self.kiwoom.d2_deposit)
+        item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+        self.tableWidget.setItem(0, 0, item)
+
+        for i in range(1, 6):
+            item = QTableWidgetItem(self.kiwoom.opw00018_output['single'][i - 1])
+            item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+            self.tableWidget.setItem(0, i, item)
+
+        self.tableWidget.resizeRowsToContents()
+
+        # Item list
+        item_count = len(self.kiwoom.opw00018_output['multi'])
+        self.tableWidget_2.setRowCount(item_count)
+
+        for j in range(item_count):
+            row = self.kiwoom.opw00018_output['multi'][j]
+            for i in range(len(row)):
+                item = QTableWidgetItem(row[i])
+                item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+                self.tableWidget_2.setItem(j, i, item)
+
+        self.tableWidget_2.resizeRowsToContents()
 
 
 if __name__ == "__main__":
